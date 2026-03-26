8ca49544795dd84d175bc034be0f345b9cb179b2
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2026-03-26 13:55:14 +0100

    Actualización de imports en UserListComponent

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,8 +1,7 @@
-import { CommonModule } from "@angular/common";
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, computed, inject, OnInit, signal } from "@angular/core";
 import { UserService } from "../../services/user.service";
 import { User } from "../../models/user";
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -18,22 +17,48 @@ import { Router } from '@angular/router'
   //       }
   // </ul>
 
-  imports: [CommonModule],
+  imports: [],
 })
+
 export class UserListComponent implements OnInit {
-  users: User[] = [];
   private userService = inject(UserService);
   private router = inject(Router);
 
+  nameFilter = signal(''); // Variables de estado
+  cityFilter = signal('');
+  users = signal<User[]>([]);
+
+  uniqueCities = computed(() => {
+    return [...new Set(this.users().map(user => user.address?.city ?? ''))].filter(city => city !== '');
+  });
+
+  filteredUsers = computed(() => { // Variable de estado computada que se actualiza automáticamente al cambiar los filtros
+    const name = this.nameFilter().toLowerCase();
+    const city = this.cityFilter(); // Viene de un select, no necesita toLowerCase()
+
+    return this.users().filter(user => {
+      const matchesName = user.name.toLowerCase().includes(name);
+      const matchesCity = city ? user.address?.city === city : true;
+      return matchesName && matchesCity;
+    });
+  });
+
   ngOnInit() {
     this.userService.getUsers().subscribe({
-      next: (data) => this.users = data,
+      next: (data) => {
+        this.users.set(data);
+      },
       error: (err) => console.error('Error al obtener usuarios', err)
     });
   }
 
   goToDetails(id: number) {
     this.router.navigate(['/details', id]);
+  }
+
+  resetFilters() {
+    this.nameFilter.set('');
+    this.cityFilter.set('');
   }
 
 }
